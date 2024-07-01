81f93b45178e5e2b7a9698a3bbeef4d076829511
Renan Augusto dos Santos <146970438+renanfrontend@users.noreply.github.com>
2024-07-01 17:14:23 +0000

    Revert "Add files via upload"
    This reverts commit 4998fc18814f2c040a3ac9a8ee08fb443bbb0945.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Adicione qualquer código JavaScript personalizado aqui
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página carregada');
 });
